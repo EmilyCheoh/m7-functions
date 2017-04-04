6774e1f30268523f5c46109880e734cbb8a075f5
EmilyCheoh <xiaoliq@uw.edu>
2017-04-04 14:49:40 -0700

    add a space

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -3,7 +3,7 @@
 # Write a function `AddThree` that adds 3 to an input value
 
 AddThree <- function(a) {
-  return (a +3 )
+  return (a + 3 )
 }
 
 # Create a variable `ten` by passing 7 to your `AddThree` function
